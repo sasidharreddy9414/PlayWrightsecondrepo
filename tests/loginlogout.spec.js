685a83b595b3df22b6ae435948ada6b6c1605f56
sasidharreddy9414 <sasidharreddy9414@gmail.com>
2025-06-15 15:32:38 +0530

    Login testscript

---
+++ tests/loginlogout.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test')
+ 
+test('Login and Logout Test Script', async({page})=>{
+    await page.goto("http://localhost/login.do")
+    await page.waitForTimeout(4000)
+ 
+    //Login Action
+    await page.locator("//input[@id='username']").fill("admin")
+    await page.locator("//input[@placeholder='Password']").fill("manager")
+    await page.locator("a[id='loginButton'] div").click()
+    await page.waitForTimeout(4000)
+ 
+})
